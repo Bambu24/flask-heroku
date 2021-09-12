7d0138165fcf82b571fec26dabf37e44dc78afcd
Bambu24 <89367530+Bambu24@users.noreply.github.com>
2021-09-12 22:12:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello นางสาวกชพร สระเกษ ชั้นม.4/8 เลขที่24"
 
 @app.route('/api', methods=['GET'])
 def get_api():
